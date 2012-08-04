98c98770dc4e1064d0b3e51c55abf8882149df90
Nicolas Artman <nicolasartman@gmail.com>
2012-08-04 01:18:50 -0700

    add paper stack (needs work) and menu item for enabling/disabling. add dummy data (in a separate file for easy removal later)

--- app.js
+++ app.js
@@ -13,43 +13,7 @@ var QuickGrade = angular.module('quickGrade', []);
 
 function Data () {
   // temp dummy data
-  var assignments = [{
-    "assignmentName": "Lab 1",
-    "questions": [
-      {
-        "question": "What would you say if someone asked you a generic placeholder question?"
-      },
-      {
-        "question": "How much?!"
-      }
-    ],
-    "submissions": [
-      {
-        "studentName": "A Student",
-        "answers": [
-          {
-            "answer": "Retro blog cliche, direct trade fingerstache keffiyeh twee. Bicycle rights blog mustache, williamsburg scenester flexitarian cred twee pour-over letterpress pork belly. Beard williamsburg authentic fanny pack bespoke helvetica. Narwhal portland DIY single-origin coffee pour-over, beard semiotics gastropub brooklyn PBR banh mi. Williamsburg thundercats ethnic, flexitarian odd future wes anderson biodiesel wayfarers sartorial craft beer dreamcatcher. Marfa cred ethnic tumblr. Forage ethical retro, four loko raw denim synth lo-fi polaroid 3 wolf moon beard quinoa fanny pack."
-          }
-        ]
-      },
-      {
-        "studentName": "Bobby Tables",
-        "answers": [
-          {
-            "answer": "Retro brooklyn flexitarian street art bespoke wolf. Trust fund carles butcher pickled chillwave. Banksy 3 wolf moon seitan kale chips, cosby sweater butcher vice small batch raw denim post-ironic beard. Mcsweeney's thundercats brooklyn, flexitarian chambray occupy 8-bit four loko. Brooklyn beard vice, lo-fi post-ironic mcsweeney's fingerstache butcher skateboard selvage pour-over twee locavore wes anderson. Keytar fap pork belly, small batch american apparel lo-fi ethnic marfa pickled tofu banksy authentic whatever twee pop-up. 3 wolf moon tattooed tofu kale chips, jean shorts skateboard pop-up retro post-ironic aesthetic."
-          }
-        ]
-      },
-      {
-        "studentName": "Jimbo",
-        "answers": [
-          {
-            "answer": "Biodiesel fingerstache whatever freegan, occupy small batch keffiyeh gentrify selvage sustainable ethical dreamcatcher 3 wolf moon. Mlkshk marfa lomo dreamcatcher, umami organic wayfarers fanny pack wolf mixtape irony aesthetic occupy fingerstache cosby sweater. DIY tattooed four loko portland butcher, letterpress swag tumblr sriracha banksy quinoa fanny pack sustainable chillwave. Scenester etsy narwhal, before they sold out typewriter chambray gentrify thundercats. Gentrify swag fanny pack hoodie pitchfork etsy. Pour-over irony helvetica polaroid stumptown mlkshk. Synth scenester locavore, sartorial squid cray helvetica bespoke sustainable odd future ethical farm-to-table VHS selvage brunch."
-          }
-        ]
-      }
-    ]
-  }];
+  var assignments = dummyData.assignments;
 
   this.dump = function () {
     console.log(assignments);
@@ -75,16 +39,15 @@ function Data () {
 QuickGrade.factory('settings', function () {
   return (function () {
     var self = {};
-  
+    
+    self.showPaperStack = true;
+    
+    self.boolToVisibilityVerb = function (bool) {
+      console.log("blarp");
+      return bool ? "Show" : "Hide";
+    }
+    
     self.menuItems = [
-      {
-        "name": "Settings",
-        "children": [
-          // {
-          //   "name": "Something"
-          // }
-        ]
-      },
       {
         "name": "Help"
       }
@@ -124,6 +87,8 @@ QuickGrade.controller('assignmentController', function ($scope, data, settings) 
   var currentQuestionNumber = 0;
 
   var currentAnswersToGrade = _.shuffle(data.getAnswersForQuestion(currentAssignment, currentQuestionNumber));
+
+  $scope.settings = settings;
 
   $scope.dump = function () {
     data.dump();
